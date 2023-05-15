dc7c1eb9b5c7e8dc6c607fc140abaf96e78d74ee
karl-fee <88248364+karl-fee@users.noreply.github.com>
2023-05-15 13:39:24 +0200

    import user data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      employees: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) =>
+      this.setState(
+        () => {
+          return { employees: users }
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.employees.map((employee) => {
+          return (
+            <div key={employee.id}>
+              <h1>{employee.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
