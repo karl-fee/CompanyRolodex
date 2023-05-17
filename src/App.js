5d66f21a38bc829fee856b98b98afd5359711d47
karl-fee <88248364+karl-fee@users.noreply.github.com>
2023-05-17 08:18:53 +0200

    add search box

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ class App extends Component {
     this.state = {
       employees: [],
     };
+
+    console.log("01-Constructor");
   }
 
   componentDidMount() {
+    console.log("03-ComponentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => response.json())
     .then((users) =>
@@ -20,15 +23,31 @@ class App extends Component {
           return { employees: users }
         },
         () => {
-          console.log(this.state);
+          //console.log(this.state);
         }
       )
     );
   }
 
   render() {
+    console.log("02-Render");
     return (
       <div className="App">
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search employees'
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredEmployees = this.state.employees.filter((employee) => {
+              return employee.name.toLocaleLowerCase().includes(searchString);
+            });
+            this.setState(() => {
+              return { employees: filteredEmployees};
+            });
+          }}
+        />
         {this.state.employees.map((employee) => {
           return (
             <div key={employee.id}>
